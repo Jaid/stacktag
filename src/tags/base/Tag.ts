4caf1b18cd0e25aa90a5b2b138bb80ecc7d231e0
Jaid <6216144+Jaid@users.noreply.github.com>
2026-05-26 03:12:03 +0100

    Added docstrings
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/tags/base/Tag.ts
+++ src/tags/base/Tag.ts
@@ -10,35 +10,65 @@ type EventPayload = {
 }
 
 export default abstract class Tag {
-  abstract detect(folder: string, partialResults: Record<string, unknown>): Promise<unknown>
+  /**
+   * A detection run that doesn’t throw will always be considered a `true` detection. Throw a `NotDetectedError` to indicate a `false` state from a clean detection run or any other `Error` to indicate a `false` state due to unexpected conditions.
+   * @return an optional payload with data collected during detection
+   */
+  abstract detect(folder: string): Promise<unknown>
   getName(): string {
     return this.constructor.name
   }
+  /**
+   * @returns priority – higher numbers get evaluated earlier
+   */
   getPriority(): number {
     return 100
   }
+  /**
+   * @return a list of tags whose detection state should be forcibly `true`
+   */
   implies(): TagList {
     return []
   }
+  /**
+   * @return a list of tags whose detection state should be `true`, furthermore those won’t have their own `detect()` called
+   */
   impliesAndSkips(): TagList {
     return []
   }
+  /**
+   * event listener that gets triggered after a `tag.detect()` was called
+   */
   listen(event: EventPayload) {
     void event
   }
+  /**
+   * @return a list of tags that should be detected before this tag gets detected – Every listed tag must have a detection state of `true`, otherwise the own detection will be skipped
+   */
   needs(): TagList {
     return []
   }
+  /**
+   * @return a list of tags that should be detected before this tag gets detected – At least one of the listed tags must have a detection state of `true`, otherwise the own detection will be skipped
+   */
   needsSome(): TagList {
     return []
   }
-  shouldRun(partialResults: Record<string, unknown>): boolean {
-    void partialResults
+  /**
+   * @return whether `detect()` should be called
+   */
+  shouldRun(): boolean {
     return true
   }
+  /**
+   * @return a list of tags that should be detected after this tag gets detected
+   */
   shouldRunAfter(): TagList {
     return []
   }
+  /**
+   * @return a list of tags that should be detected before this tag gets detected
+   */
   shouldRunBefore(): TagList {
     return []
   }
